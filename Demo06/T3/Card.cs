320342b1b2444f4fa32ab84b4e1e211ce76ab6da
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-02-01 22:36:04 +0200

    Initial commit.

---
+++ Demo06/T3/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class Card
+    {
+        public string Land { get; set; }
+        public int CardNumber { get; set; }
+        public string CardName { get; set; }
+
+        public override string ToString()
+        {
+            return "Card Land: " + Land + " Card number: " + CardNumber + " Card name: " + CardName;
+        }
+    }
+}
---
+++ Demo06/T3/Deck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class Deck
+    {
+        public string DeckName { get; set; }
+
+        private List<Card> cards;
+
+        public Deck()
+        {
+            cards = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+
+        public void PrintCollection()
+        {
+            Console.WriteLine("Cards:");
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.ToString());
+            }
+        }
+        public override string ToString()
+        {
+            return "Name of the card deck: " + DeckName;
+        }
+    }
+}
